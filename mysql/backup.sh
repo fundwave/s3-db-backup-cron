a12bd18e1ccce83c830be9e825c5a062bacdbd19
rickygarg <rickygarg@gmail.com>
2026-05-14 13:00:19 +0530

    fix : double slash while uploading mysql backups to s3
    when PREFIX already had a trailing slash

--- mysql/backup.sh
+++ mysql/backup.sh
@@ -1,27 +1,27 @@
-#!/bin/bash        
-                                    
+#!/bin/bash
+
 FILE=${1:-'backup'}
-BUCKET=${2}                                                                                                                                                             
-FILE_PATH=$(dirname $FILE_PATH$FILE)                                                                                                                                    
-FILE_PATH="${FILE_PATH}""/"                                                                                                                                             
-                                                                                                                                                                        
-echo "$FILE_PATH""$FILE"                                                                                                                                                
-TAR_MD5_SUM=$(openssl md5 -binary "$FILE_PATH$FILE".tar.gz | base64)                                                                                                    
-SQL_MD5_SUM=$(openssl md5 -binary $FILE_PATH$FILE".sql" | base64)                                                                                                       
+BUCKET=${2}
+FILE_PATH=$(dirname $FILE_PATH$FILE)
+FILE_PATH="${FILE_PATH}""/"
+
+echo "$FILE_PATH""$FILE"
+TAR_MD5_SUM=$(openssl md5 -binary "$FILE_PATH$FILE".tar.gz | base64)
+SQL_MD5_SUM=$(openssl md5 -binary $FILE_PATH$FILE".sql" | base64)
 #REMOTE_SQL_MD5_SUM=$(aws s3api head-object --bucket $BUCKET --key "$FILE".tar.gz --query 'Metadata.sqlmd5checksum' --output text)
 REMOTE_SQL_MD5_SUM=$(aws s3api head-object --bucket $BUCKET --key "${S3_PREFIX}$FILE".tar.gz --query 'Metadata.sqlmd5checksum' --output text 2>&1) || (if grep -q "An error occurred (404) when calling the HeadObject operation: Not Found" <<< "$REMOTE_SQL_MD5_SUM"; then echo "$REMOTE_SQL_MD5_SUM"; else echo "$REMOTE_SQL_MD5_SUM" 1>&2; fi)
 
-echo $SQL_MD5_SUM                                                                                                                                                       
-echo $REMOTE_SQL_MD5_SUM                                                                                                                                                
-                                                                                                                                                                        
-if [[ "$SQL_MD5_SUM" == "$REMOTE_SQL_MD5_SUM" ]]                                                                                                                        
-  then                                                                                                                                                                  
-    echo "No changes since last upload. Quitting."                                                                                                                      
-    exit 0                                                                                                                                                              
-fi                                                                                                                                                                      
-                                                                                                                                                                        
-echo "Changes found since last upload. Uploading now."                                                                                                                  
-                                                                                                                                                                        
-aws s3api put-object --bucket $BUCKET --key "${S3_PREFIX:+${S3_PREFIX}/}$FILE".tar.gz --body "$FILE_PATH$FILE".tar.gz --content-md5 $TAR_MD5_SUM --metadata sqlmd5checksum=$SQL_MD5_SUM             
-                                                                                                                                                                        
-echo "Backup complete"                                                                                                                                                  
+echo $SQL_MD5_SUM
+echo $REMOTE_SQL_MD5_SUM
+
+if [[ "$SQL_MD5_SUM" == "$REMOTE_SQL_MD5_SUM" ]]
+  then
+    echo "No changes since last upload. Quitting."
+    exit 0
+fi
+
+echo "Changes found since last upload. Uploading now."
+
+aws s3api put-object --bucket $BUCKET --key "${S3_PREFIX%/}/$FILE".tar.gz --body "$FILE_PATH$FILE".tar.gz --content-md5 $TAR_MD5_SUM --metadata sqlmd5checksum=$SQL_MD5_SUM
+
+echo "Backup complete"
